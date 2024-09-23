39a4290c3812425f3080ddc65d3f76f3d02eba88
Kevin Chan <knchan@dons.usfca.edu>
2024-09-23 16:58:34 -0700

    Added tool functs, need to test with algos

--- mars_planner.py
+++ mars_planner.py
@@ -17,11 +17,12 @@ from copy import deepcopy
 from search_algorithms import breadth_first_search
 
 class RoverState :
-    def __init__(self, loc="station", sample_extracted=False, holding_sample=False, charged=False):
+    def __init__(self, loc="station", sample_extracted=False, holding_sample=False, charged=False, holding_tool=False):
         self.loc = loc
         self.sample_extracted=sample_extracted
         self.holding_sample = holding_sample
         self.charged=charged
+        self.holding_tool = holding_tool
         self.prev = None
 
     ## you do this.
@@ -31,6 +32,7 @@ class RoverState :
             and self.sample_extracted == other.sample_extracted
             and self.holding_sample == other.holding_sample
             and self.charged == other.charged
+            and self.holding_tool == other.holding_tool
        )
 
 
@@ -38,7 +40,8 @@ class RoverState :
         return (f"Location: {self.loc}\n" +
                 f"Sample Extracted?: {self.sample_extracted}\n"+
                 f"Holding Sample?: {self.holding_sample}\n" +
-                f"Charged? {self.charged}")
+                f"Charged? {self.charged}\n" +
+                f"Holding Tool? {self.holding_tool}")
 
     def __hash__(self):
         return self.__repr__().__hash__()
@@ -74,6 +77,25 @@ def move_to_battery(state) :
     return r2
 # add tool functions here
 
+def pick_up_tool(state) :
+    r2 = deepcopy(state)
+    r2.holding_tool = True
+    r2.prev = state
+    return r2
+
+
+def drop_tool(state) :
+    r2 = deepcopy(state)
+    r2.holding_tool = False
+    r2.prev = state
+    return r2
+
+def use_tool(state) :
+    r2 = deepcopy(state)
+    if state.holding_tool and state.loc == "sample":
+        r2.sample_extracted = True
+    r2.prev = state
+    return r2
 
 def pick_up_sample(state) :
     r2 = deepcopy(state)
